9f046fd5d9987b4e660617263659d799fc9e9f67
dennis <info.dennis.durmus@gmail.com>
2024-11-04 14:52:35 +0100

    Add  three Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:timer_app/timer_button.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(
+            "Stopwatch",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TimerButton(text: "Start"),
+              SizedBox(width: 10),
+              TimerButton(text: "Stop"),
+              SizedBox(width: 10),
+              TimerButton(text: "Clear"),
+            ],
+          ),
         ),
       ),
     );
