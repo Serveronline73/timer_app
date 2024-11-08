5d3775deacc1ab4f8f1243c9d04487071cf81c8a
dennis <info.dennis.durmus@gmail.com>
2024-11-08 10:22:44 +0100

    Button Funktion Sowie Style bearbeitet

--- lib/timer_button.dart
+++ lib/timer_button.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class TimerButton extends StatelessWidget {
-  const TimerButton({super.key, required this.text});
+  const TimerButton(
+      {super.key,
+      required this.textColor,
+      required this.text,
+      required this.onTap,
+      required this.color});
+
   final String text;
+  final Function()? onTap;
+  final Color color;
+  final Color textColor;
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: color, foregroundColor: textColor),
       child: Text(text),
     );
   }
